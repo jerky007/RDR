72a4f08619abe64aea57ba6c7c1848f7cb8c41e5
Andy Morris <ajmorris@gmail.com>
2011-06-05 16:15:19 -0500

    added express and got some basic user functions working

---
+++ server.js
@@ -0,0 +1,48 @@
+require.paths.unshift('support/mongoose/lib')
+var fs = require('fs')
+var express = require('express')
+
+var app = express.createServer();
+app.use(express.bodyParser());
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+
+var User = new Schema({
+  id          : ObjectId,
+  first_name  : String,
+  last_name   : String,
+  date_created : Date
+});
+
+var UserModel = mongoose.model('User', User);
+
+app.post('/user/create', function(req, res){
+  new UserModel(req.body).save(function(err){
+    res.send('user saved successfully');
+    res.send(err);
+  });
+});
+
+app.get('/users.json', function(req, res)
+{
+  UserModel.find({}, function(err, users){
+   res.send(users); 
+  });
+});
+
+app.get('/test_page', function(req, res){
+   res.header("Content-Type", "text/html");
+   res.send(fs.readFileSync('test_page.html'));
+ });
+ 
+app.get('/test_client.js', function(req, res){
+  res.header("Content-Type", "text/javascript");
+  res.send(fs.readFileSync('test_client.js'));
+});
+
+app.listen(3000);
+
---
+++ test_client.js
@@ -0,0 +1,9 @@
+$(function()
+{
+  $("#create_user").click(function(){
+    $.post("/user/create", { first_name: "Andy", last_name: "Morris" },
+       function(data) {
+         $("#output").append(data)
+       });
+  });
+});
