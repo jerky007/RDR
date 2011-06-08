ea362a0b86453d2cb7d7f2d78f3d6ee0629d9b5d
Andy Morris <ajmorris@gmail.com>
2011-06-07 20:03:05 -0500

    ..

--- db.js
+++ db.js
@@ -2,26 +2,27 @@ var db = module.exports = require('mongoose');
 
 db.connect('mongodb://localhost/test');
 
+(function setupModels()
+{
+  var Schema = db.Schema
+    , ObjectId = Schema.ObjectId;
 
-var Schema = db.Schema
-  , ObjectId = Schema.ObjectId;
+  var Location = new Schema({
+    user_id     : ObjectId,
+    latitude    : Number,
+    longitude   : Number
+  });
 
-var Location = new Schema({
-  user_id     : ObjectId,
-  latitude    : Number,
-  longitude   : Number
-});
-
-var User = new Schema({
-  id          : ObjectId,
-  first_name  : String,
-  last_name   : String,
-  date_created : Date
-});
-
-db.model('Location', Location);
-db.model('User', User);
-
+  var User = new Schema({
+    id          : ObjectId,
+    first_name  : String,
+    last_name   : String,
+    date_created : Date
+  });
+  
+  db.model('Location', Location);
+  db.model('User', User);
+})();
 
 
 
